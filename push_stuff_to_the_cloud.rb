371067a5530cfd72c2d1cd4a2972b528998ae870
Evan R <eunoia.github@gmail.com>
2014-10-09 18:13:14 -0700

    module that saves rendered html to S3

---
+++ push_stuff_to_the_cloud.rb
@@ -0,0 +1,18 @@
+module PushStuffToTheCloud
+  def saveToS3(html)
+    connection = Fog::Storage.new({
+      :provider => "AWS",
+      :endpoint => "http://s3.amazonaws.com",
+      :region   => "us-west-1",
+      :aws_access_key_id => ENV["AWS_ACCESS_KEY_ID"],
+      :aws_secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]
+    })
+    directory = connection.directories.get("www.soonestdmvappointment.com")
+    file = directory.files.create(
+      :key    => "index.html",
+      :body   => html,
+      :public => true
+    )
+    file.public_url
+  end
+end
